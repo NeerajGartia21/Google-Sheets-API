75c111107d9eecb317b3b15183f89c71a25b9430
Neeraj Gartia <neerajgartia211002@gmail.com>
2022-05-13 08:39:18 +0530

    added env sample

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -5,6 +5,8 @@ const {googleAuth,fetchSheetData,updateData}=require('../controller')
 
 
 router.get('/login',googleAuth);
+//  the /login route is for fetching the access token from google
+//  int the frontend the developer has to add this url as the callback url for the google auth client
 router.get('/spreadsheet/:id',verifyToken,fetchSheetData);
 router.post('/spreadsheet/update',verifyToken,updateData);
 
